9c968a29c8e19148c2db2425aa9a561ead5bec2d
崔兵兵 <1655902151@qq.com>
2017-03-16 13:57:20 +0800

    自定义swift网络请求方式<post>

--- DYDemo/DYDemo/Classes/Home/Controller/HomeViewController.swift
+++ DYDemo/DYDemo/Classes/Home/Controller/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 private let kTitleViewH : CGFloat = 40
 
---
+++ DYDemo/DYDemo/Classes/Tool/NetworkTools.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkTools.swift
+//  DYDemo
+//
+//  Created by 这个夏天有点冷 on 2017/3/15.
+//  Copyright © 2017年 YLT. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+enum MethodType {
+    case get
+    case post
+}
+
+class NetworkTools {
+    class func requestData(_ type : MethodType, URLString : String, parameters : [String : Any]? = nil, finishedCallback : @escaping (_ result : Any) -> ()) {
+        
+        //  1.获取类型
+        let method = type == .get ? HTTPMethod.get : HTTPMethod.post
+        
+        //  2.发送网络请求
+        Alamofire.request(URLString, method: method, parameters: parameters).responseJSON { (response) in
+            
+            //  3.获取结果
+            //  ?? 空合运算符
+            guard let result = response.result.value else {
+                print(response.result.error ?? "服务器返回数据错误")
+                return
+            }
+            
+            // 4.将结果回调出去
+            finishedCallback(result)
+        }
+    }
+}
